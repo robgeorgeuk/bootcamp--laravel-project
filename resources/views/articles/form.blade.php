d1943544e110447d38220e4a10b4cea4026b7261
Mark Wales <mark@smallhadroncollider.com>
2020-02-10 16:16:35 +0000

    update: updates form styling

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -5,44 +5,50 @@
 }}@endsection
 
 @section("content")
-    <h2>{{ $title }}</h2>
 
-    <form method="post" class="form">
-        @csrf
+    <form method="post" class="form card">
+        <h2 class="card-header">{{ $title }}</h2>
 
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input
-                id="title"
-                name="title"
-                class="form-control @error('title') is-invalid @enderror"
-                value="{{ old("title", $article ? $article->title : "") }}"
-            />
+        <fieldset class="card-body">
+            @csrf
 
-            @error('title')
-                <p class="invalid-feedback">
-                    {{ $message }}
-                </p>
-            @enderror
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input
+                    id="title"
+                    name="title"
+                    class="form-control @error('title') is-invalid @enderror"
+                    value="{{ old("title", $article ? $article->title : "") }}"
+                />
+
+                @error('title')
+                    <p class="invalid-feedback">
+                        {{ $message }}
+                    </p>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea
+                    id="content"
+                    name="content"
+                    class="form-control @error('content') is-invalid @enderror"
+                >{{
+                    old("content", $article ? $article->content : "")
+                }}</textarea>
+
+                @error('content')
+                    <p class="invalid-feedback">
+                        {{ $message }}
+                    </p>
+                @enderror
+            </div>
+
+        </fieldset>
+
+        <div class="card-footer text-right">
+            <button class="btn btn-success">{{ $button }}</button>
         </div>
-
-        <div class="form-group">
-            <label for="content">Content</label>
-            <textarea
-                id="content"
-                name="content"
-                class="form-control @error('content') is-invalid @enderror"
-            >{{
-                old("content", $article ? $article->content : "")
-            }}</textarea>
-
-            @error('content')
-                <p class="invalid-feedback">
-                    {{ $message }}
-                </p>
-            @enderror
-        </div>
-
-        <button class="btn btn-success">{{ $button }}</button>
     </form>
 @endsection
