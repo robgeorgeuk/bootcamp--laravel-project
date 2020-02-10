c22de7a1e2ebbb7abb35138a008cbbc84f8a491f
Mark Wales <mark@smallhadroncollider.com>
2020-02-10 16:46:40 +0000

    update: adds auth check to ArticleRequest

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ArticleRequest extends FormRequest
@@ -13,7 +14,7 @@ class ArticleRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Auth::check();
     }
 
     /**
