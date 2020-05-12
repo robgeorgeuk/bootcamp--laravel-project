92cadb7a75e4c8be67630c5c581d84663aaab4b9
Rob George <rob@robgeorge.co.uk>
2020-05-12 14:59:47 +0100

    Capistrano config fix: typo in server name.

--- deployment/deploy/production.rb
+++ deployment/deploy/production.rb
@@ -1,4 +1,4 @@
-server "http://ec2-3-10-212-58.eu-west-2.compute.amazonaws.com", user: "ubuntu", roles: %w{app}
+server "ec2-3-10-212-58.eu-west-2.compute.amazonaws.com", user: "ubuntu", roles: %w{app}
 
 set :branch, "master"
 set :deploy_to, "/var/www/bootcamp--laravel-project"
